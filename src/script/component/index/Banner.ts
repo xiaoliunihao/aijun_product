2b26c1aeb938913532d9e9adf146c90402871b9c
yuzhouzhongdetaiyang <3434740689@qq.com>
2017-04-22 14:10:28 +0800

    myproduct

---
+++ src/script/component/index/Banner.ts
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+export default class Banner extends Component{
+	render(){
+		return (
+			<div>
+			   <img src=""/>
+			</div>
+		)
+	}
+}
--- src/script/component/index/Content.js
+++ src/script/component/index/Content.js
@@ -4,9 +4,7 @@ export default class Content extends Component{
 	render(){
 		return(
 			<div className="index_content">
-			首页内容
-			   <span><Link to="/StoreDetail/?id=1">进入商品详情页面1</Link></span>
-			   <span><Link to="/StoreDetail/?id=2">进入商品详情页面2</Link></span>
+				
 			</div>
 		)
 	}
