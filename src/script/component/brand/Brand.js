2afbe701380465e74e8de42ddeade64f420f12bc
myzhuaijun <2641815425@qq.com>
2017-04-25 23:11:12 +0800

    20170425zhu

--- src/script/component/brand/Brand.js
+++ src/script/component/brand/Brand.js
@@ -3,11 +3,10 @@ import React,{Component} from "react";
 import BrandList from "./BrandList";
 
 
-
 export default class Brand extends Component{
 	render(){
 		return(
-			<div className="m-brand">
+			<div className="m-brand" >
 				<div className="brandFabric">
 					<img src="http://www.fanjiangdz.com/weixin/newpublic/images/wxmater01.jpg"/>
 				</div>
--- src/script/component/login/Login.js
+++ src/script/component/login/Login.js
@@ -6,6 +6,13 @@ export default class Login extends Component{
 				<div className="Login_img">
 					<img src="http://www.fanjiangdz.com/weixin/newpublic/images/logobgImg.png"/>
 				</div>
+				<div className="PeronMess">
+					<input type="text" placeholder="手机号"  className="PhoneNum"/>
+					<input type="text" placeholder="验证码" className="Password"/>
+				</div>
+				<div className="Login_Mess">
+					<button className="login">登录</button>
+				</div>
 			</div>
 		)
 	}
